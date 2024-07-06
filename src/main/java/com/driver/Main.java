ae90e292971d7c318efa4a9fe14b2fd4a1e2e32d
sangmalasurywanshi935@gamil.com <sangmalasurywanshi935@gmail.com>
2024-07-06 08:40:43 +0530

    get name successfully

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+
+    public static void main(String[] args){
+        RWOnly obj = new RWOnly();
+        obj.setName("Sangmala");
+        String ans = obj.getName();
+        System.out.println(ans);
+    }
 }
